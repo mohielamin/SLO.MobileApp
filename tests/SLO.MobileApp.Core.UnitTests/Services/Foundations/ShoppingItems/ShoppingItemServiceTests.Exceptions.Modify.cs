094bf922f8a3186313c835039c06d3f261213548
Mohammed Elamin <mohammed.qahwagi@outlook.com>
2026-03-13 17:52:55 +0100

    ShouldThrowDependencyValidationExceptionOnModifyIfDbUpdateConcurrencyErrorOccursAndLogItAsync -> FAIL

---
+++ tests/SLO.MobileApp.Core.UnitTests/Services/Foundations/ShoppingItems/ShoppingItemServiceTests.Exceptions.Modify.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using SLO.MobileApp.Core.Models.Foundations.ShoppingItems;
+using SLO.MobileApp.Core.Models.Foundations.ShoppingItems.Exceptions;
+using SLO.MobileApp.Core.UnitTests.Helpers;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SLO.MobileApp.Core.UnitTests.Services.Foundations.ShoppingItems;
+
+public partial class ShoppingItemServiceTests
+{
+    [Fact]
+    public async Task ShouldThrowDependencyValidationExceptionOnModifyIfDbUpdateConcurrencyErrorOccursAndLogItAsync()
+    {
+        // given
+        DateTimeOffset currentDateTime = Randomizers.GetRandomDateTime();
+
+        ShoppingItem randomShoppingItem =
+            CreateRandomShoppingItem(currentDateTime);
+
+        randomShoppingItem.UpdatedAt =
+            randomShoppingItem.UpdatedAt.AddMinutes(1);
+
+        var exceptionMessage = Randomizers.GetRandomString();
+
+        var dbUpdateConcurrencyException =
+            new DbUpdateConcurrencyException(exceptionMessage);
+
+        var lockedShoppingItemException =
+            new LockedShoppingItemException(
+                exceptionMessage: "Locked shopping item error occurred, " +
+                "try again please!",
+                innerException: dbUpdateConcurrencyException);
+
+        var expectedShoppingItemDependencyValidationException =
+            new ShoppingItemDependencyValidationException(
+                exceptionMessage: "Shopping item dependency validation error occurred, " +
+                "try again please!",
+                innerException: lockedShoppingItemException);
+
+        _dateTimeBrokerMock.Setup(broker =>
+            broker.GetCurrentDateTimeAsync(
+                It.IsAny<CancellationToken>()))
+            .ThrowsAsync(dbUpdateConcurrencyException);
+
+        // when
+        ValueTask<ShoppingItem> modifyShoppingItemTask =
+            _shoppingItemService.ModifyShoppingItemAsync(
+                randomShoppingItem,
+                It.IsAny<CancellationToken>());
+
+        await Assert.ThrowsAsync<ShoppingItemDependencyValidationException>(
+            modifyShoppingItemTask.AsTask);
+
+        // then
+        _dateTimeBrokerMock.Verify(broker =>
+            broker.GetCurrentDateTimeAsync(
+                It.IsAny<CancellationToken>()),
+            Times.Once());
+
+        _storageBrokerMock.Verify(broker =>
+            broker.SelectShoppingItemByIdAsync(
+                It.IsAny<Guid>(),
+                It.IsAny<CancellationToken>()),
+            Times.Never());
+
+        _storageBrokerMock.Verify(broker =>
+            broker.UpdateShoppingItemAsync(
+                It.IsAny<ShoppingItem>(),
+                It.IsAny<CancellationToken>()),
+            Times.Never());
+
+        _loggingBrokerMock.Verify(broker =>
+            broker.LogErrorAsync(
+                It.Is(Randomizers.SameExceptionAs(
+                    expectedShoppingItemDependencyValidationException))),
+            Times.Once());
+
+        VerifyNoOtherDependencyCalls();
+    }
+}
