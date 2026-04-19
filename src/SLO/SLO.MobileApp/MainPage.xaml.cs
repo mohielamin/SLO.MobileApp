4b52588b66725f9dcd8c020728a1e32bd0edab5a
MOHi <62817205+mohielamin@users.noreply.github.com>
2026-04-19 13:43:54 +0200

    BASES: Set Padding to Zero on Button Base (#28)
    * BASES: Set Padding to Zero on Button Base
    Closes #24
    
    * CODERUB: Cleanup

--- src/SLO/SLO.MobileApp/MainPage.xaml.cs
+++ src/SLO/SLO.MobileApp/MainPage.xaml.cs
@@ -1,43 +1,12 @@
 ﻿using Microsoft.Maui.Controls;
-using SLO.MobileApp.Core.Models.Foundations.ShoppingItems;
-using System;
-using System.ComponentModel;
 
 namespace SLO.MobileApp;
 
-public partial class MainPage : ContentPage, INotifyPropertyChanged
+public partial class MainPage : ContentPage
 {
-    private ShoppingItem _shoppingItem;
-
-    public ShoppingItem ShoppingItem
-    {
-        get { return _shoppingItem; }
-        set
-        {
-            _shoppingItem = value;
-            OnPropertyChanged();
-        }
-    }
-
-
-    int count = 0;
 
     public MainPage()
     {
         InitializeComponent();
-
-        ShoppingItem =
-            new ShoppingItem
-            {
-                CreatedAt = DateTime.Now,
-                CreatedBy = Guid.NewGuid(),
-                Description = "Desc",
-                Name = "New",
-                Quantity = 123
-            };
-
-        BindingContext = this;
     }
-
-#nullable enable
 }
