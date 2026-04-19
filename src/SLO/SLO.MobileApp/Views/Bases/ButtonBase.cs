62faf6cd57fce247596d7d70b88b8536eae6d40f
MOHi <mohammed.qahwagi@outlook.com>
2026-04-19 13:32:39 +0200

    BASES: Set Padding to Zero on Button Base Closes #24

--- src/SLO/SLO.MobileApp/Views/Bases/ButtonBase.cs
+++ src/SLO/SLO.MobileApp/Views/Bases/ButtonBase.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.Maui.Controls;
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
 
 namespace SLO.MobileApp.Views.Bases;
 
 public partial class ButtonBase : Button
 {
+    public ButtonBase()
+    {
+        this.Padding = Thickness.Zero;
+    }
 }
