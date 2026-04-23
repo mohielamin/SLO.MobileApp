403be897a6fb5c2420a1cb6f6d2e32dd2eba3bf6
MOHi <mohammed.qahwagi@outlook.com>
2026-04-23 20:16:42 +0200

    INFRA: Create Button Base and Templated View Base

---
+++ src/SLO/SLO.MobileApp/Controls/Bases/ButtonBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace SLO.MobileApp.Controls.Bases;
+
+public partial class ButtonBase : Button
+{
+    public ButtonBase()
+    {
+        this.Padding = Thickness.Zero;
+    }
+}
---
+++ src/SLO/SLO.MobileApp/Controls/TemplatedViews/TemplatedViewBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace SLO.MobileApp.Controls.TemplatedViews;
+
+public abstract partial class TemplatedViewBase : TemplatedView
+{
+    internal static BindableProperty CreateProperty<PropertyType, DeclaringType>(
+        string propertyName,
+        object defaultValue = null,
+        BindingMode defaultBindingMode = BindingMode.OneWay,
+        BindableProperty.BindingPropertyChangedDelegate propertyChangedDelegate = null,
+        BindableProperty.BindingPropertyChangingDelegate propertyChangingDelegate = null) =>
+        BindableProperty.Create(
+            propertyName,
+            returnType: typeof(PropertyType),
+            declaringType: typeof(DeclaringType),
+            defaultValue,
+            defaultBindingMode,
+            propertyChanged: propertyChangedDelegate,
+            propertyChanging: propertyChangingDelegate);
+
+    internal static BindablePropertyKey CreateReadOnlyProperty<PropertyType, DeclaringType>(
+        string propertyName,
+        object defaultValue = null) =>
+        BindableProperty.CreateReadOnly(
+            propertyName,
+            returnType: typeof(PropertyType),
+            declaringType: typeof(DeclaringType),
+            defaultValue);
+}
