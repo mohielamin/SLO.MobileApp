f20feef304fa832628b02e472ef905bdbfdd3a84
MOHi <mohammed.qahwagi@outlook.com>
2026-04-25 16:02:33 +0200

    FIXES: Prevent None Digits On Numeric Entry Control

--- src/SLO/SLO.MobileApp/Controls/Entries/NumericEntry.xaml.cs
+++ src/SLO/SLO.MobileApp/Controls/Entries/NumericEntry.xaml.cs
@@ -1,3 +1,6 @@
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using SLO.MobileApp.Controls.Bases;
 using SLO.MobileApp.Controls.TemplatedViews;
 
 namespace SLO.MobileApp.Controls.Entries;
@@ -7,5 +10,32 @@ public partial class NumericEntry : TemplatedEntryView
     public NumericEntry()
     {
         InitializeComponent();
+    }
+
+    private void TextChangedEvent(
+        object sender, TextChangedEventArgs e)
+    {
+        if (sender is not EntryBase entryBase)
+        {
+            return;
+        }
+
+        if (entryBase.Keyboard != Keyboard.Numeric)
+        {
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(e.NewTextValue))
+        {
+            return;
+        }
+
+        bool isParsed =
+            int.TryParse(s: e.NewTextValue, out _);
+
+        if (isParsed is false)
+        {
+            entryBase.Text = e.OldTextValue;
+        }
     }
 }
