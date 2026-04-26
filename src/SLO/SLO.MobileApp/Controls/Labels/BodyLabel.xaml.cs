cc994dd316dbb0b96dd70321102605b92ec7054c
MOHi <mohammed.qahwagi@outlook.com>
2026-04-26 12:52:02 +0200

    CONTROLS: Create BodyLabel
    CLOSES #41

---
+++ src/SLO/SLO.MobileApp/Controls/Labels/BodyLabel.xaml.cs
@@ -0,0 +1,11 @@
+using SLO.MobileApp.Controls.TemplatedViews;
+
+namespace SLO.MobileApp.Controls.Labels;
+
+public partial class BodyLabel : TemplatedLabelView
+{
+    public BodyLabel()
+    {
+        InitializeComponent();
+    }
+}
