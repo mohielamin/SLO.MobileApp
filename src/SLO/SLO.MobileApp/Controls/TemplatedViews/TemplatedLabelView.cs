78fba5027c6a8c309e6ddb04d4e0bf24a38d4ac7
MOHi <62817205+mohielamin@users.noreply.github.com>
2026-04-26 14:49:46 +0200

    Users/mohielamin/controls/41 controls body label (#46)
    * BASE CONTROLS: Create Label Base
    
    CLOSES #42
    
    * BASE CONTROLS: Create Templated Label View
    
    CLOSES #43
    
    * CONTROLS: Create BodyLabel
    
    CLOSES #41
    
    * CODERUB:
    - Remove/Replace old Label Base
    - Cleanup the SLO.MobileApp.Views.Bases namepace
    
    CLOSES #44
    
    * DESIGNS: Include Label Design Abstractions
    
    CLOSES #45
    
    * CODERUB: Cleanup

---
+++ src/SLO/SLO.MobileApp/Controls/TemplatedViews/TemplatedLabelView.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+
+namespace SLO.MobileApp.Controls.TemplatedViews;
+
+public abstract partial class TemplatedLabelView : TemplatedViewBase
+{
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public Color TextColor
+    {
+        get => (Color)GetValue(TextColorProperty);
+        set => SetValue(TextColorProperty, value);
+    }
+
+    public static readonly BindableProperty TextProperty =
+        CreateProperty<string, TemplatedLabelView>(
+            propertyName: nameof(Text));
+
+    public static readonly BindableProperty TextColorProperty =
+        CreateProperty<Color, TemplatedLabelView>(
+            propertyName: nameof(TextColor));
+}
--- src/SLO/SLO.MobileApp/Views/Bases/LabelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.Maui.Controls;
-
-namespace SLO.MobileApp.Views.Bases;
-
-internal partial class LabelBase : Label
-{
-}
