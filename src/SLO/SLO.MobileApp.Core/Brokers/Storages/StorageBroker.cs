8a3d4e410e77c694c81201c4288d2b4ec6bdc07c
MOHi <mohammed.qahwagi@outlook.com>
2026-04-28 19:27:22 +0200

    INFRA: Create UseSloMobileAppCore
    CLOSES #11

--- src/SLO/SLO.MobileApp.Core/Brokers/Storages/StorageBroker.cs
+++ src/SLO/SLO.MobileApp.Core/Brokers/Storages/StorageBroker.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using EFxceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SLO.MobileApp.Core.Models.Configurations;
+using System;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace SLO.MobileApp.Core.Brokers.Storages;
 
-internal sealed partial class StorageBroker : IStorageBroker
+internal sealed partial class StorageBroker : EFxceptionsContext, IStorageBroker
 {
+    private readonly IOptions<LocalConfiguration> _localConfiguration;
+
+    public StorageBroker(IOptions<LocalConfiguration> localConfiguration)
+    {
+        _localConfiguration = localConfiguration;
+        EnsureCreated();
+    }
+
+    protected override void OnConfiguring(
+        DbContextOptionsBuilder dbContextOptionsBuilder)
+    {
+        string connectionString =
+            $"Data Source={_localConfiguration.Value.DatabaseFilePath}";
+
+        dbContextOptionsBuilder.UseSqlite(connectionString);
+    }
+
+    private void EnsureCreated()
+    {
+        bool databaseFileExists =
+            File.Exists(path: _localConfiguration.Value.DatabaseFilePath);
+
+        if (databaseFileExists)
+        {
+            return;
+        }
+
+        Database.EnsureCreated();
+    }
+
     private async ValueTask<T> InsertAsync<T>(
         T item, CancellationToken cancellationToken) =>
         throw new NotImplementedException();
--- src/SLO/SLO.MobileApp/MauiProgram.cs
+++ src/SLO/SLO.MobileApp/MauiProgram.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Maui.Controls.Hosting;
 using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Storage;
+using SLO.MobileApp.Core.DependencyInjections;
 
 namespace SLO.MobileApp;
 
@@ -17,6 +19,9 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
+
+        mauiAppBuilder.Services.UseSloMobileAppCore(
+            appDataDirectory: FileSystem.AppDataDirectory);
 
 #if DEBUG
         mauiAppBuilder.Logging.AddDebug();
