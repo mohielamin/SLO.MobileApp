3d6a021cf5f3f53ce43b490a474f1a0fb39741e2
MOHi <mohammed.qahwagi@outlook.com>
2026-05-03 21:33:02 +0200

    ShouldThrowValidationExceptionOnAddIfShoppingListItemIsNullAndLogItAsync -> FAIL

---
+++ src/SLO/SLO.MobileApp.Core/Models/Foundations/ShoppingListItems/Exceptions/ShoppingListItemValidationException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SLO.MobileApp.Core.Models.Foundations.ShoppingListItems.Exceptions;
+
+public class ShoppingListItemValidationException : Exception
+{
+    public ShoppingListItemValidationException(
+        string exceptionMessage,
+        Exception innerException)
+        : base(exceptionMessage, innerException) { }
+}
---
+++ tests/SLO.MobileApp.Core.UnitTests/Services/Foundations/ShoppingListItems/ShoppingListItemServiceTests.Validations.Add.cs
@@ -0,0 +1,52 @@
+﻿using Moq;
+using SLO.MobileApp.Core.Models.Foundations.ShoppingListItems;
+using SLO.MobileApp.Core.Models.Foundations.ShoppingListItems.Exceptions;
+using SLO.MobileApp.Core.UnitTests.Helpers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SLO.MobileApp.Core.UnitTests.Services.Foundations.ShoppingListItems;
+
+public partial class ShoppingListItemServiceTests
+{
+    [Fact]
+    public async Task ShouldThrowValidationExceptionOnAddIfShoppingListItemIsNullAndLogItAsync()
+    {
+        // given
+        ShoppingListItem nullShoppingListItem = null;
+
+        var nullShoppingListItemException =
+            new NullShoppingListItemException(
+                exceptionMessage: "Shopping list item is null.");
+
+        var expectedShoppingListItemValidationException =
+            new ShoppingListItemValidationException(
+                exceptionMessage: "Shopping list item validation error occurred, " +
+                "fix the errors and try again please!",
+                innerException: nullShoppingListItemException);
+
+        // when
+        ValueTask<ShoppingListItem> addShoppingListItemTask =
+            _shoppingListItemService.AddShoppingListItemAsync(
+                nullShoppingListItem,
+                It.IsAny<CancellationToken>());
+
+        await Assert.ThrowsAsync<ShoppingListItemValidationException>(
+            addShoppingListItemTask.AsTask);
+
+        // then
+        _storageBrokerMock.Verify(broker =>
+            broker.InsertShoppingListItemAsync(
+                It.IsAny<ShoppingListItem>(),
+                It.IsAny<CancellationToken>()),
+            Times.Once());
+
+        _loggingBrokerMock.Verify(broker =>
+            broker.LogErrorAsync(
+                It.Is(Randomizers.SameExceptionAs(
+                    expectedShoppingListItemValidationException))),
+            Times.Once());
+
+        VerifyNoOtherDependencyCalls();
+    }
+}
