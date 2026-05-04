8c0db87a8db798247a8a9663cb64315f65b27f37
MOHi <mohammed.qahwagi@outlook.com>
2026-05-04 09:56:07 +0200

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ src/SLO/SLO.MobileApp.Core/Models/Foundations/ShoppingListItems/Exceptions/ShoppingListItemDependencyException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SLO.MobileApp.Core.Models.Foundations.ShoppingListItems.Exceptions;
+
+public class ShoppingListItemDependencyException : Exception
+{
+    public ShoppingListItemDependencyException(
+        string exceptionMessage,
+        Exception innerException)
+        : base(exceptionMessage, innerException) { }
+}
