0b0b3cdab5779cb7c1121f2b2ffaa536e79e541e
MOHi <mohammed.qahwagi@outlook.com>
2026-05-05 19:02:03 +0200

    ShouldThrowDependencyExceptionOnRemoveByIdIfDbUpdateErrorOccursAndLogItAsync -> PASS

--- tests/SLO.MobileApp.Core.UnitTests/Services/Foundations/ShoppingListItems/ShoppingListItemServiceTests.Exceptions.RemoveById.cs
+++ tests/SLO.MobileApp.Core.UnitTests/Services/Foundations/ShoppingListItems/ShoppingListItemServiceTests.Exceptions.RemoveById.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
 using SLO.MobileApp.Core.Models.Foundations.ShoppingListItems;
 using SLO.MobileApp.Core.Models.Foundations.ShoppingListItems.Exceptions;
 using SLO.MobileApp.Core.UnitTests.Helpers;
@@ -10,6 +11,63 @@ namespace SLO.MobileApp.Core.UnitTests.Services.Foundations.ShoppingListItems;
 
 public partial class ShoppingListItemServiceTests
 {
+    [Fact]
+    public async Task ShouldThrowDependencyExceptionOnRemoveByIdIfDbUpdateErrorOccursAndLogItAsync()
+    {
+        // given
+        Guid someShoppingListItemId = Guid.NewGuid();
+        string exceptionMessage = Randomizers.GetRandomString();
+        var dbUpdateException = new DbUpdateException(exceptionMessage);
+
+        var failedShoppingListItemStorageException =
+            new FailedShoppingListItemStorageException(
+                exceptionMessage: "Failed shopping list item storage error occurred, " +
+                "please contact support.",
+                innerException: dbUpdateException);
+
+        var expectedShoppingListItemDependencyException =
+            new ShoppingListItemDependencyException(
+                exceptionMessage: "Shopping list item dependency error occurred, " +
+                "please contact support.",
+                innerException: failedShoppingListItemStorageException);
+
+        _storageBrokerMock.Setup(broker =>
+            broker.SelectShoppingListItemByIdAsync(
+                someShoppingListItemId,
+                It.IsAny<CancellationToken>()))
+            .ThrowsAsync(dbUpdateException);
+
+        // when
+        ValueTask<ShoppingListItem> removeShoppingListItemByIdTask =
+            _shoppingListItemService.RemoveShoppingListItemByIdAsync(
+                someShoppingListItemId,
+                It.IsAny<CancellationToken>());
+
+        // then
+        await Assert.ThrowsAsync<ShoppingListItemDependencyException>(
+            removeShoppingListItemByIdTask.AsTask);
+
+        _storageBrokerMock.Verify(broker =>
+            broker.SelectShoppingListItemByIdAsync(
+                someShoppingListItemId,
+                It.IsAny<CancellationToken>()),
+            Times.Once());
+
+        _storageBrokerMock.Verify(broker =>
+            broker.DeleteShoppingListItemAsync(
+                It.IsAny<ShoppingListItem>(),
+                It.IsAny<CancellationToken>()),
+            Times.Never());
+
+        _loggingBrokerMock.Verify(broker =>
+            broker.LogErrorAsync(
+                It.Is(Randomizers.SameExceptionAs(
+                    expectedShoppingListItemDependencyException))),
+            Times.Once());
+
+        VerifyNoOtherDependencyCalls();
+    }
+
     [Fact]
     public async Task ShouldThrowCriticalDependencyExceptionOnRemoveByIdIfSqlErrorOccursAndLogItAsync()
     {
