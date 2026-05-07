a187e5b376d81391673eac3c5832ece5efede6ce
MOHi <mohammed.qahwagi@outlook.com>
2026-05-07 10:49:32 +0200

    ShouldThrowServiceExceptionOnRetrieveAllIfServiceErrorOccursAsync -> FAIL

--- tests/SLO.MobileApp.Core.UnitTests/Services/Foundations/ShoppingLists/ShoppingListServiceTests.Exceptions.RetrieveAll.cs
+++ tests/SLO.MobileApp.Core.UnitTests/Services/Foundations/ShoppingLists/ShoppingListServiceTests.Exceptions.RetrieveAll.cs
@@ -2,6 +2,7 @@
 using SLO.MobileApp.Core.Models.Foundations.ShoppingLists;
 using SLO.MobileApp.Core.Models.Foundations.ShoppingLists.Exceptions;
 using SLO.MobileApp.Core.UnitTests.Helpers;
+using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -51,6 +52,54 @@ public partial class ShoppingListServiceTests
             broker.LogCriticalAsync(
                 It.Is(Randomizers.SameExceptionAs(
                     expectedShoppingListDependencyException))),
+            Times.Once());
+
+        VerifyNoOtherDependencyCalls();
+    }
+
+    [Fact]
+    public async Task ShouldThrowServiceExceptionOnRetrieveAllIfServiceErrorOccursAsync()
+    {
+        // given
+        var sqlException = Randomizers.GetSqlException();
+        string exceptionMessage = Randomizers.GetRandomString();
+        var someServiceException = new Exception(exceptionMessage);
+
+        var failedShoppingListServiceException =
+            new FailedShoppingListServiceException(
+                exceptionMessage: "Failed shopping list service error occurred, " +
+                "please contact support.",
+                innerException: someServiceException);
+
+        var expectedShoppingListServiceException =
+            new ShoppingListServiceException(
+                exceptionMessage: "Shopping list service error occurred, " +
+                "please contact support.",
+                innerException: failedShoppingListServiceException);
+
+        _storageBrokerMock.Setup(broker =>
+            broker.SelectAllShoppingListsAsync(
+                It.IsAny<CancellationToken>()))
+            .ThrowsAsync(someServiceException);
+
+        // when
+        ValueTask<IQueryable<ShoppingList>> retrieveAllShoppingListsTask =
+            _shoppingListService.RetrieveAllShoppingListsAsync(
+                It.IsAny<CancellationToken>());
+
+        // then
+        await Assert.ThrowsAsync<ShoppingListServiceException>(
+            retrieveAllShoppingListsTask.AsTask);
+
+        _storageBrokerMock.Verify(broker =>
+            broker.SelectAllShoppingListsAsync(
+                It.IsAny<CancellationToken>()),
+            Times.Once());
+
+        _loggingBrokerMock.Verify(broker =>
+            broker.LogErrorAsync(
+                It.Is(Randomizers.SameExceptionAs(
+                    expectedShoppingListServiceException))),
             Times.Once());
 
         VerifyNoOtherDependencyCalls();
