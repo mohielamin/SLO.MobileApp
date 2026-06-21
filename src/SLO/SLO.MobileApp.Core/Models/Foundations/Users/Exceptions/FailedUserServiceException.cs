45584d2e2b7e1a167631994ba4fc0f986e927a71
MOHi <mohammed.qahwagi@outlook.com>
2026-06-21 20:14:19 +0200

    ShouldThrowServiceExceptionOnRetrieveLoggedInUserIfServiceErrorOccursAndLogItAsync -> FAIL

---
+++ src/SLO/SLO.MobileApp.Core/Models/Foundations/Users/Exceptions/FailedUserServiceException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SLO.MobileApp.Core.Models.Foundations.Users.Exceptions;
+
+public class FailedUserServiceException : Exception
+{
+    public FailedUserServiceException(
+        string exceptionMessage,
+        Exception innerException)
+       : base(exceptionMessage, innerException) { }
+}
---
+++ tests/SLO.MobileApp.Core.UnitTests/Services/Foundations/Users/UserServiceTests.Exceptions.RetrieveLoggedInUser.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+using SLO.MobileApp.Core.Models.Foundations.Users.Exceptions;
+using SLO.MobileApp.Core.UnitTests.Helpers;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SLO.MobileApp.Core.UnitTests.Services.Foundations.Users;
+
+public partial class UserServiceTests
+{
+    [Fact]
+    public async Task ShouldThrowServiceExceptionOnRetrieveLoggedInUserIfServiceErrorOccursAndLogItAsync()
+    {
+        // given
+        string exceptionMessage = Randomizers.GetRandomString();
+        var someServiceException = new Exception(exceptionMessage);
+
+        var failedUserServiceException =
+            new FailedUserServiceException(
+                exceptionMessage: "Failed user service error occurred, " +
+                "please contact support.",
+                innerException: someServiceException);
+
+        var expectedUserServiceException =
+            new UserServiceException(
+                exceptionMessage: "User service error occurred, " +
+                "please contact support.",
+                innerException: failedUserServiceException);
+
+        _userManagementBrokerMock.Setup(broker =>
+            broker.GetLoggedInUserIdAsync(
+                It.IsAny<CancellationToken>()))
+            .ThrowsAsync(someServiceException);
+
+        // when
+        ValueTask<Guid> retrieveLoggedInUserAsyncTask =
+            _userService.RetrieveLoggedInUserAsync(
+                It.IsAny<CancellationToken>());
+
+        // then
+        await Assert.ThrowsAsync<UserServiceException>(
+            retrieveLoggedInUserAsyncTask.AsTask);
+
+        _userManagementBrokerMock.Verify(broker =>
+            broker.GetLoggedInUserIdAsync(
+                It.IsAny<CancellationToken>()));
+
+        _loggingBrokerMock.Verify(broker =>
+            broker.LogErrorAsync(
+                It.Is(Randomizers.SameExceptionAs(
+                    expectedUserServiceException))),
+            Times.Once());
+
+        VerifyNoOtherDependencyCalls();
+    }
+}
