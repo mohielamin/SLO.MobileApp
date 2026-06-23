ade07992d6053a66eeba5ccbf57b259e2b453877
MOHi <mohammed.qahwagi@outlook.com>
2026-06-23 11:25:22 +0200

    ShouldRetrieveLoggedInUserAsync -> FAIL

---
+++ src/SLO/SLO.MobileApp.Core/Services/Processings/Users/UserProcessingService.cs
@@ -0,0 +1,25 @@
+﻿using SLO.MobileApp.Core.Brokers.Loggings;
+using SLO.MobileApp.Core.Services.Foundations.Users;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SLO.MobileApp.Core.Services.Processings.Users;
+
+internal class UserProcessingService : IUserProcessingService
+{
+    private readonly IUserService _userService;
+    private readonly ILoggingBroker _loggingBroker;
+
+    public UserProcessingService(
+        IUserService userService,
+        ILoggingBroker loggingBroker)
+    {
+        _userService = userService;
+        _loggingBroker = loggingBroker;
+    }
+
+    public async ValueTask<Guid> RetrieveLoggedInUserAsync(
+        CancellationToken cancellationToken) =>
+        throw new NotImplementedException();
+}
---
+++ tests/SLO.MobileApp.Core.UnitTests/Services/Processings/Users/UserProcessingServiceTests.Logic.RetrieveLoggedInUser.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Moq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SLO.MobileApp.Core.UnitTests.Services.Processings.Users;
+
+public partial class UserProcessingServiceTests
+{
+    [Fact]
+    public async Task ShouldRetrieveLoggedInUserAsync()
+    {
+        // given
+        Guid randomId = Guid.NewGuid();
+        Guid retrievedLoggedInUser = randomId;
+        Guid expectedLoggedInUser = retrievedLoggedInUser;
+
+        _userServiceMock.Setup(broker =>
+            broker.RetrieveLoggedInUserAsync(
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(retrievedLoggedInUser);
+
+        // when
+        Guid actualLoggedInUser =
+            await _userProcessingService.RetrieveLoggedInUserAsync(
+                It.IsAny<CancellationToken>());
+
+        // then
+        actualLoggedInUser.Should().Be(expectedLoggedInUser);
+
+        _userServiceMock.Verify(broker =>
+            broker.RetrieveLoggedInUserAsync(
+                It.IsAny<CancellationToken>()),
+            Times.Once());
+
+        VerifyNoOtherDependencyCalls();
+    }
+}
