071333546d3900d7a8d6037ad7cbf48728e841b2
MOHi <mohammed.qahwagi@outlook.com>
2026-06-26 10:08:21 +0200

    ShouldAddShoppingListItemIfShoppingListItemDoesNotExistsAsync -> PASS

--- src/SLO/SLO.MobileApp.Core/Services/Processings/ShoppingListItems/ShoppingListItemProcessingService.cs
+++ src/SLO/SLO.MobileApp.Core/Services/Processings/ShoppingListItems/ShoppingListItemProcessingService.cs
@@ -1,7 +1,6 @@
 ﻿using SLO.MobileApp.Core.Brokers.Loggings;
 using SLO.MobileApp.Core.Models.Foundations.ShoppingListItems;
 using SLO.MobileApp.Core.Services.Foundations.ShoppingListItems;
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -22,6 +21,13 @@ internal class ShoppingListItemProcessingService : IShoppingListItemProcessingSe
 
     public async ValueTask<ShoppingListItem> UpsertShoppingListItemAsync(
         ShoppingListItem shoppingListItem,
-        CancellationToken cancellationToken) =>
-        throw new NotImplementedException();
+        CancellationToken cancellationToken)
+    {
+        await _shoppingListItemService.RetrieveAllShoppingListItemsAsync(
+            cancellationToken);
+
+        return await _shoppingListItemService.AddShoppingListItemAsync(
+            shoppingListItem,
+            cancellationToken);
+    }
 }
