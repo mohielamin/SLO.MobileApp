2c233ca0dc39108838e2386ae3d78202d3b75517
MOHi <mohammed.qahwagi@outlook.com>
2026-06-27 11:14:25 +0200

    ShouldThrowDependencyExceptionOnRemoveByIdIfDependencyErrorOccursAndLogItAsync -> PASS

--- tests/SLO.MobileApp.Core.UnitTests/Services/Processings/ShoppingListItems/ShoppingListItemProcessingServiceTests.Exceptions.RemoveById.cs
+++ tests/SLO.MobileApp.Core.UnitTests/Services/Processings/ShoppingListItems/ShoppingListItemProcessingServiceTests.Exceptions.RemoveById.cs
@@ -54,4 +54,49 @@ public partial class ShoppingListItemProcessingServiceTests
 
         VerifyNoOtherDependencyCalls();
     }
+
+    [Theory]
+    [MemberData(nameof(DependencyExceptions))]
+    public async Task ShouldThrowDependencyExceptionOnRemoveByIdIfDependencyErrorOccursAndLogItAsync(
+        Exception dependencyException)
+    {
+        // given
+        Guid someShoppingListItemId = Guid.NewGuid();
+
+        var exepctedShoppingListItemProcessingDependencyException =
+            new ShoppingListItemProcessingDependencyException(
+                exceptionMessage: "Shopping list item processing dependency error occurred, " +
+                "please contact support.",
+                innerException: dependencyException);
+
+        _shoppingListItemServiceMock.Setup(service =>
+            service.RemoveShoppingListItemByIdAsync(
+                someShoppingListItemId,
+                It.IsAny<CancellationToken>()))
+            .ThrowsAsync(dependencyException);
+
+        // when
+        ValueTask<ShoppingListItem> removeByIdAsyncTask =
+            _shoppingListItemProcessingService.RemoveShoppingListItemByIdAsync(
+                someShoppingListItemId,
+                It.IsAny<CancellationToken>());
+
+        // then
+        await Assert.ThrowsAsync<ShoppingListItemProcessingDependencyException>(
+            removeByIdAsyncTask.AsTask);
+
+        _shoppingListItemServiceMock.Verify(service =>
+            service.RemoveShoppingListItemByIdAsync(
+                someShoppingListItemId,
+                It.IsAny<CancellationToken>()),
+            Times.Once());
+
+        _loggingBrokerMock.Verify(broker =>
+            broker.LogErrorAsync(
+                It.Is(Randomizers.SameExceptionAs(
+                    exepctedShoppingListItemProcessingDependencyException))),
+            Times.Once());
+
+        VerifyNoOtherDependencyCalls();
+    }
 }
