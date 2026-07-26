2899a724f3752b61c3c6a4a804523a896b0cc555
MOHi <mohammed.qahwagi@outlook.com>
2026-07-26 18:32:57 +0200

    ShouldRetrieveAllShoppingListItemsByIdAsync -> FAIL

---
+++ src/SLO/SLO.MobileApp.Core/Services/Coordinations/ShoppingLists/ShoppingListCoordinationService.cs
@@ -0,0 +1,34 @@
+﻿using SLO.MobileApp.Core.Brokers.Loggings;
+using SLO.MobileApp.Core.Models.Foundations.ShoppingListItems;
+using SLO.MobileApp.Core.Services.Processings.ShoppingListItems;
+using SLO.MobileApp.Core.Services.Processings.ShoppingLists;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SLO.MobileApp.Core.Services.Coordinations.ShoppingLists;
+
+internal class ShoppingListCoordinationService : IShoppingListCoordinationService
+{
+    private readonly IShoppingListProcessingService _shoppingListProcessingService;
+    private readonly IShoppingListItemProcessingService _shoppingListItemProcessingService;
+    private readonly ILoggingBroker _loggingBroker;
+
+    public ShoppingListCoordinationService(
+        IShoppingListProcessingService shoppingListProcessingService,
+        IShoppingListItemProcessingService shoppingListItemProcessingService,
+        ILoggingBroker loggingBroker)
+    {
+        _shoppingListProcessingService = shoppingListProcessingService;
+        _shoppingListItemProcessingService = shoppingListItemProcessingService;
+        _loggingBroker = loggingBroker;
+    }
+
+    public async ValueTask<IQueryable<ShoppingListItem>> RetrieveAllShoppingListItemsByIdAsync(
+        Guid shoppingListId,
+        CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ tests/SLO.MobileApp.Core.UnitTests/Services/Coordinations/ShoppingLists/ShoppingListCoordinationServiceTests.Logic.RetreiveAllListItemsById.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using SLO.MobileApp.Core.Models.Foundations.ShoppingListItems;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SLO.MobileApp.Core.UnitTests.Services.Coordinations.ShoppingLists;
+
+public partial class ShoppingListCoordinationServiceTests
+{
+    [Fact]
+    public async Task ShouldRetrieveAllShoppingListItemsByIdAsync()
+    {
+        // given
+        Guid someShoppingListId = Guid.NewGuid();
+        IQueryable<ShoppingListItem> randomShoppingListItems =
+            CreateRandomShoppingListItems(shoppingListId: someShoppingListId);
+
+        IQueryable<ShoppingListItem> retrieveShoppingListItems =
+            randomShoppingListItems;
+
+        IQueryable<ShoppingListItem> expectedShoppingListItems =
+            retrieveShoppingListItems.DeepClone();
+
+        _shoppingListItemProcessingServiceMock.Setup(service =>
+            service.RetrieveAllShoppingListItemsByShoppingListIdAsync(
+                someShoppingListId,
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(retrieveShoppingListItems);
+
+        // when
+        IQueryable<ShoppingListItem> actualShoppingListItems =
+            await _shoppingListCoordinationService.RetrieveAllShoppingListItemsByIdAsync(
+                shoppingListId: someShoppingListId,
+                cancellationToken: It.IsAny<CancellationToken>());
+        // then
+        actualShoppingListItems.Should().BeEquivalentTo(
+            expectedShoppingListItems);
+
+        _shoppingListItemProcessingServiceMock.Verify(service =>
+            service.RetrieveAllShoppingListItemsByShoppingListIdAsync(
+                someShoppingListId,
+                It.IsAny<CancellationToken>()),
+            Times.Once());
+
+        VerifyNotOtherDependencyCalls();
+    }
+}
